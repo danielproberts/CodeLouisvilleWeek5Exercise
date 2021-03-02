41338378c19948720a00afa54cd6ad221a6543d4
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-03-02 10:15:26 -0500

    Add project files.

---
+++ CodeLouisvilleWeek5Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeLouisvilleWeek5Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
