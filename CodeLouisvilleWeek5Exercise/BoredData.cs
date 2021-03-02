6f1a8c250571dc13e6ba0e2723f0df6aafe64ce8
Daniel Roberts <daniel.prescott.roberts@gmail.com>
2021-03-02 11:28:49 -0500

    demo ready

---
+++ CodeLouisvilleWeek5Exercise/BoredData.cs
@@ -0,0 +1,45 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace CodeLouisvilleWeek5Exercise
+{
+    class BoredData
+    {
+        //JSON Properties
+        public string Activity { get; set; }
+        public string Type { get; set; }
+        public int Participants { get; set; }
+
+        //Get Data From API
+        public BoredData pullData(string url)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(url);
+                BoredData responseData = JsonConvert.DeserializeObject<BoredData>(json);
+                return responseData;
+            }
+        }
+    }
+    public static class StringExtensions
+    {
+        public static string FirstCharToUpper(this string input) =>
+            input switch
+            {
+                null => throw new ArgumentNullException(nameof(input)),
+                "" => throw new ArgumentException($"{nameof(input)} cannot be empty", nameof(input)),
+                _ => input.First().ToString().ToUpper() + input.Substring(1)
+            };
+
+        public static void CenterTitle(string s)
+        {
+            string windowHeader = s;
+            Console.SetCursorPosition((Console.WindowWidth - windowHeader.Length) / 2, Console.CursorTop);
+            Console.WriteLine(windowHeader);
+        }
+    }
+}
--- CodeLouisvilleWeek5Exercise/Program.cs
+++ CodeLouisvilleWeek5Exercise/Program.cs
@@ -6,7 +6,26 @@ namespace CodeLouisvilleWeek5Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string url = @"https://www.boredapi.com/api/activity";
+            BoredData data = new BoredData();
+            data = data.pullData(url);
+
+            Console.WriteLine();
+            StringExtensions.CenterTitle("BoredAPI Activity Suggestion");
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write("Name of Activity: ");
+            Console.WriteLine(data.Activity);
+
+            Console.Write("Type of Activity: ");
+            Console.WriteLine(StringExtensions.FirstCharToUpper(data.Type));
+            //Console.WriteLine(data.Type);
+
+
+            Console.Write("Number of Participants: ");
+            Console.WriteLine(data.Participants);
+            Console.ReadKey();
         }
     }
 }
